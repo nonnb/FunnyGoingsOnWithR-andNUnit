987168f172b6af08e023a719df7fc67ffedc7f35
StuartLC <nonamebrande@gmail.com>
2013-09-26 14:16:46 +0200

    To repeat , need to debug these unit tests with R# 7.1.3 c# Ed (2224) NUnit 2.6.1.12217

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace ClassLibrary1
+{
+   [TestFixture]
+   public class SomeTest
+   {
+      [Test]
+      public static void TestCase1()
+      {
+         var postParameters = new Dictionary<string, string>
+            {
+               {"someNumber", "100"},
+               {"someString", "Hello World"}
+            };
+
+         var resultWhenInConditional =
+            (postParameters != null)
+               ? postParameters
+                    .Keys
+                    .Zip(postParameters.Values,
+                         (key, value) => string.Format("{0}={1}", key, value))
+                    .Aggregate<string, string>(null,
+                                               (prev, next) =>
+                                               (prev != null)
+                                                  ? string.Format("{0}&{1}", prev, next)
+                                                  : next)
+               : string.Empty;
+
+         Assert.IsNotNull(resultWhenInConditional, "Result is NULL!");
+      }
+
+      [Test]
+      public static void TestCase2()
+      {
+         Dictionary<string, string> postParameters = null;
+
+         var resultWhenInConditional =
+            (postParameters != null)
+               ? postParameters
+                    .Keys
+                    .Zip(postParameters.Values,
+                         (key, value) => string.Format("{0}={1}", key, value))
+                    .Aggregate<string, string>(null,
+                                               (prev, next) =>
+                                               (prev != null)
+                                                  ? string.Format("{0}&{1}", prev, next)
+                                                  : next)
+               : string.Empty;
+
+         Assert.IsNotNull(resultWhenInConditional, "Result is NULL!");
+      }
+
+      [Test]
+      public static void TestCase3()
+      {
+         Assert.IsNotNull(AsParam(null), "Result is NULL!");
+      }
+
+      [Test]
+      public static void TestCase4()
+      {
+         var postParameters = new Dictionary<string, string>
+            {
+               {"someNumber", "100"},
+               {"someString", "Hello World"}
+            };
+         var result = AsParam(postParameters);
+         Assert.IsNotNull(result, "Result is NULL!");
+      }
+
+      public static string AsParam(IDictionary<string, string> postParameters)
+      {
+         var resultWhenInConditional =
+            (postParameters != null)
+               ? postParameters
+                    .Keys
+                    .Zip(postParameters.Values,
+                         (key, value) => string.Format("{0}={1}", key, value))
+                    .Aggregate<string, string>(null,
+                                               (prev, next) =>
+                                               (prev != null)
+                                                  ? string.Format("{0}&{1}", prev, next)
+                                                  : next)
+               : string.Empty;
+
+         // Place break point here
+         return resultWhenInConditional;
+      }
+   }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassLibrary1;
+
+namespace ConsoleApplication1
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         SomeTest.TestCase1();
+         SomeTest.TestCase2();
+         SomeTest.AsParam(null);
+      }
+   }
+}
